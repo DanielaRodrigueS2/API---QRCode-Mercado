16f14b106cebb65dc352b994ecc11adeb1f14c2a
Daniela <danielarodrigues2po@gmail.com>
2026-04-25 21:45:56 -0300

    Criação estrutural do carrinho

--- my-app/app/Carrinho.tsx
+++ my-app/app/Carrinho.tsx
@@ -1,6 +1,8 @@
-import { View, Text, Pressable } from "react-native"
+import { View, Text, Pressable, StyleSheet } from "react-native"
 import { useRouter } from "expo-router"
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import Botao from "@/components/Botao";
 
 
 export default function Carrinho(){
@@ -8,13 +10,56 @@ export default function Carrinho(){
     const router = useRouter();
 
     return(
-        <View>  
-            <Text>Carrinho</Text>
-            <Pressable onPress={() => router.navigate('/Home')}>
-                <Text>
-                    Clique aqui
-                </Text>
-            </Pressable>
-        </View>
+        <SafeAreaView style={styles.principal}>  
+
+            <View style={styles.header}>
+                <Text></Text>
+            </View>
+
+            <View style={styles.main}>
+
+            </View>
+
+            <View style={styles.footer}>
+                <Botao onPress={() => router.navigate('/Carrinho')} nome="cart"></Botao>
+                <Botao  onPress={() => router.navigate('/Home')} nome="camera"></Botao>
+                <Botao nome="settings"></Botao>
+            </View>
+            
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    principal: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
+    },
+
+    header:{
+        flex: 0.1,
+        width: '100%',
+        alignItems: 'center',
+        backgroundColor: 'black'
+    },
+    main:{
+        flex: 0.8,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        backgroundColor: '#919191'
+    },
+    footer:{
+        flex: 0.1,
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        backgroundColor: 'black'
+    },
+
+
+})
