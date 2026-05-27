f442e15283ac3ff022f303dbcb269850c9e62f84
Daniela <danielarodrigues2po@gmail.com>
2026-05-26 22:13:50 -0300

    Pagina de compras criada

---
+++ my-app/app/Compra.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+
+
+type Produtos = {
+    nome: string,
+    valor: string,
+    unidade: string,
+    quantidade: string
+}
+
+export default function Compra(props : Produtos){
+
+
+
+    return(
+        <View>
+
+        </View>
+    )
+
+
+}
