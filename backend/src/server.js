fd95bd88be7ca3aedd183b035da1475653cd358d
Daniela <danielarodrigues2po@gmail.com>
2026-06-07 20:15:25 -0300

    criação backend

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+const espress = require('express');
+const cors = require('cors ');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000, () =>{
+    console.log('Iniciou o server')
+})
