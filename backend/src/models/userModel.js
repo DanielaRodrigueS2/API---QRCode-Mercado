84ff4ef19a7ee2582f8445d5550248f7899edb79
Daniela <danielarodrigues2po@gmail.com>
2026-06-09 10:45:18 -0300

    alteração nome dos arquivos

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const NotaFiscal = require('./notaFiscal');
+const NotaFiscal = require('./notaFiscalModel');
 
 const User = new mongoose.Schema({
     nome:{
