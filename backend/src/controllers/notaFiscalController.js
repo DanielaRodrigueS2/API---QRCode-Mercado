3ae227e0f458e3db1e8664826afa3fb49994c2db
Daniela <danielarodrigues2po@gmail.com>
2026-06-10 13:27:51 -0300

    notaFiscal Controller update

--- backend/src/controllers/notaFiscalController.js
+++ backend/src/controllers/notaFiscalController.js
@@ -5,12 +5,19 @@ const Extractor = require('../extractors/extractor1');
 
 exports.gerarNotaFiscal = async (req, res) =>{
     const {urlNota} = req.body;
+    const {userId} = req.user.id;
 
     try{
         const dadosNota = await Extractor.extrairDados(urlNota)
 
         if(!dadosNota) return res.status(400).json({erro: 'Url ou código incorretor, extração falhou'});
+        const {linkAcesso, local, dataEmissao, cnpj, endereco, valorTotal, produtos} = dadosNota;
 
+        const notaFiscal = new Nota({linkAcesso, local, dataEmissao, cnpj, endereco, valorTotal, produtos});
+        await notaFiscal.save();
+
+        await User.findByIdAndUpdate(userId, {$addToSet: {notas: {$each: [notaFiscal]}}});
+        res.status(201).json({message: 'nota criada e salva com sucesso'});
 
     }
     catch(erro){
