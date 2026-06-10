4a2662c48c8b8398098b82a786099ccf325da867
Daniela <danielarodrigues2po@gmail.com>
2026-06-10 12:55:27 -0300

    atualização product model

--- backend/src/models/productModel.js
+++ backend/src/models/productModel.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     nome: String,
-    precoUnitario: Number,
+    qtd: Number,
     precoTotal: Number,
-    descricao: String,
     unidade: String,
-    quantidade: Number,
-    codigo: String
 })
 
 const Product = mongoose.model('Product', productSchema);
