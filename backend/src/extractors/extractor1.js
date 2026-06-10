e3da795e363ef3316f0dff4a2c199dcd6b4f9efd
Daniela <danielarodrigues2po@gmail.com>
2026-06-10 11:39:51 -0300

    extractor 1 finalizado

--- backend/src/extractors/extractor1.js
+++ backend/src/extractors/extractor1.js
@@ -1,12 +1,33 @@
 const {chromium} = require('playwright');
 
 exports.extrairDados = async (url) =>{
-    const navegador = chromium.launch();
+    const navegador = await chromium.launch();
     const page = await navegador.newPage();
 
     await page.goto(url);
 
-    const produtos = await page.getByRole('table').allTextContents();
-    console.log(produtos);
-    
+    const locator = await page.locator('tr').filter({has: page.getByRole('cell')}).allInnerTexts();
+    const nome = await page.getByRole('heading').filter({has: page.locator('b')}).innerText();
+
+    const notaFiscal = {
+        local: nome,
+        cnpj : locator[0].slice(6,15),
+        endereco : locator[1],
+        produtos: []
+    };
+
+    for(let i=2; i < locator.length; i++){
+        let produto = locator[i].split('\t');
+        let produtoFinal = {
+            nome: produto[0].slice(0,-18),
+            qtd: parseFloat(produto[1].slice(21,).replace(',', '.')),
+            un: produto[2].slice(4,),
+            valorTotal: parseFloat(produto[3].slice(19,).replace(',', '.'))
+        }
+
+        notaFiscal.produtos.push(produtoFinal);
+    }
+    console.log(notaFiscal);
+    await navegador.close();
+    return notaFiscal;
 }   
