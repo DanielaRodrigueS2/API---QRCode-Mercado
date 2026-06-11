aa465d0a3e74ed80820c90453bb6d429ca0ba988
Daniela <danielarodrigues2po@gmail.com>
2026-06-11 13:04:32 -0300

    Rotas User/Auth

---
+++ backend/src/routes/AuthRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const UserController = require('../controllers/userController');
+const router = express.Router();
+const {body} = require('express-validator');
+
+router.post('/register', [
+    body('nome').notEmpty(),
+    body("email").isEmail(),
+    body('senha').isLength({min: 6, max: 30})
+], UserController.register);
+
+router.post('/login', UserController.login);
+
+
