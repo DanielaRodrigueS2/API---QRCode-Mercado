db91c8d9bec567a6a32290d9c0f7dd4ed2b4530d
Daniela <danielarodrigues2po@gmail.com>
2026-06-11 13:09:01 -0300

    Nota fiscal routes

---
+++ backend/src/routes/NotasRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const NotaController = require('../controllers/notaFiscalController');
+const Auth = require('../middlewares/auth');
+
+router.post('/gerarNota', Auth, NotaController.gerarNotaFiscal);
+router.get('/retornaNotas', Auth, NotaController.getAllNotas);
+router.put('/exlcuiNota', Auth, NotaController.deleteNotaById);
+
